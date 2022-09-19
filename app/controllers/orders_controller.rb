15a86e092289d0a97eca6b76a8fb27859a21e5da
proskurekov <proskurekovdim@gmail.com>
2022-09-19 18:15:18 +0700

    Изменил routes.rb, оставил там только вложенные ресурсы clients => orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,10 +1,13 @@
 class OrdersController < ApplicationController
+  before_action :set_order, only: %i[show edit destroy update]
+
   def index
     @orders = Order.all.where("client_id == #{params[:client_id]}")
   end
 
   def new
     @order = Order.new 
+    @client = Client.new 
   end
 
   def create
@@ -20,16 +23,12 @@ class OrdersController < ApplicationController
   end
   
   def show 
-    @order = Order.find(params[:id])
   end
 
   def edit
-    @order = Order.find(params[:id])
   end
 
   def update 
-    @order = Order.find(params[:id]) 
-
     if @order.update order_params 
       redirect_to client_orders_path(@order.client_id) 
     else 
@@ -38,15 +37,20 @@ class OrdersController < ApplicationController
   end
 
   def destroy
-    @order = Order.find(params[:id])
     @order.destroy 
     redirect_to client_orders_path(@order)
   end
 
-    private
+  private
+
+  def set_order 
+    @order = Order.find(params[:id])
+  end
 
   def order_params
-    params.require(:order).permit(:problem, :work_description, :price, :client_id)
+    params.
+      require(:order).
+      permit(:problem, :work_description, :price, :client_id)
   end
 
 end
