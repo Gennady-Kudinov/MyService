65183c802c6d6ce41e998a112528874a50ccce8b
Gennady-Kudinov <gennady71@gmail.com>
2022-09-23 21:46:04 +0300

    поправлен внешний вид

--- db/migrate/20220918072144_add_status_to_order.rb
+++
@@ -1,5 +0,0 @@
-class AddStatusToOrder < ActiveRecord::Migration[7.0]
-  def change
-    add_column :orders, :status, :boolean, default: false 
-  end
-end
--- db/migrate/20220919054440_change_ecm_in_car.rb
+++
@@ -1,5 +0,0 @@
-class ChangeEcmInCar < ActiveRecord::Migration[7.0]
-  def change
-    change_column_null :cars, :ecm_id, true 
-  end
-end
