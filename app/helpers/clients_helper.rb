6047c01e859b423040c2f0b071a5276173512548
proskurekov <proskurekovdim@gmail.com>
2022-10-04 23:18:34 +0700

    Добавил хэлпер client_car

--- app/helpers/clients_helper.rb
+++ app/helpers/clients_helper.rb
@@ -1,2 +1,10 @@
 module ClientsHelper
+  
+  def client_car(client) 
+    first_car = client.cars.first 
+    
+    return 'Машины нет' if first_car.nil? 
+    first_car.make.name + ' ' +  first_car.model.name
+  end
+
 end
