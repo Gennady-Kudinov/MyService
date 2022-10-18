f6758ba795fdd4a287bef07ad6318f38d3444d7a
Gennady-Kudinov <gennady71@gmail.com>
2022-10-18 18:37:31 +0300

    Изменена папка для сохранения вставленных картинок

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -20,9 +20,16 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    # "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "BAZA/Image/#{model.id}"
   end
 
+  # Метод изменения адреса расположения прикрепленных картинок к ордеру
+  # def cache_dir
+  #   "BAZA/Image/#{model.id}"
+  # end
+
+  # Защита, белый список расширений доступных для загрузки
   def extension_whitelist
     %w(jpg jpeg gif png)
   end
