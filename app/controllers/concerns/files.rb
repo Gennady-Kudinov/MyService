f5b04c8224e3a00ca32aca478adc41e13e58ac2a
proskurekov <proskurekovdim@gmail.com>
2022-10-19 19:52:25 +0700

    мелкие правки

--- app/controllers/concerns/files.rb
+++ app/controllers/concerns/files.rb
@@ -1,7 +1,8 @@
 module Files 
   extend ActiveSupport::Concern 
   attr_accessor :file_name  
-
+  
+  # загрузка файла 
   def download_files 
     send_file(current_path, filename: @file_name)
   end
@@ -12,11 +13,12 @@ module Files
     path + search_file_name
   end
   
-  # определяем подходящий файл 
+  # определяем подходящий файл здесь будет поиск 
   def search_file_name 
     @file_name = "First_folder.zip"
   end
   
+  # если файл найден true  
   def file_found? 
     true 
   end
