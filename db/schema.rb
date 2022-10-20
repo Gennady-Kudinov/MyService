ae0c593c2d36d344a161919aeadb87d61fd99276
proskurekov <proskurekovdim@gmail.com>
2022-10-20 16:39:19 +0700

    Добавил поле email для user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_17_162820) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_20_093800) do
   create_table "articles", force: :cascade do |t|
     t.string "name"
     t.text "title"
@@ -108,6 +108,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_17_162820) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "admin", default: false
+    t.string "email"
   end
 
   add_foreign_key "cars", "clients"
