d51fd5e1cf1565fe9c4467b455e8a33d5033c09d
Gennady <108209569+Gennady-Kudinov@users.noreply.github.com>
2022-10-23 11:02:26 +0300

    В хелпере Client  заменил один метод

--- app/helpers/clients_helper.rb
+++ app/helpers/clients_helper.rb
@@ -1,17 +1,24 @@
 module ClientsHelper
   
-  def client_car(client) 
-    first_car = client.cars.first 
+ # def client_car(client) 
+ #   first_car = client.cars.first 
     
-    return 'Машины нет' if first_car.nil? 
-    first_car.make.name + ' ' +  first_car.model.name
-  end
+ #   return 'Машины нет' if first_car.nil? 
+ #   first_car.make.name + ' ' +  first_car.model.name
+ # end
 
   def mileage_km(client) 
     first_mileage = client.cars.first 
     
     return 'пробег не известен' if first_mileage.nil? 
     first_mileage.mileage
+  end
+  
+  def link(client)
+    first_car = client.cars.first 
+
+    return link_to "Добавить а/м", new_car_path if first_car.nil?
+    first_car.make.name + ' ' +  first_car.model.name 
   end
 
 end
