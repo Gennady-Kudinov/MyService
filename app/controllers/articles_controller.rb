c8bb894574f04df1f43c5fe3c638955713adcd84
Gennady-Kudinov <gennady71@gmail.com>
2022-10-27 22:59:39 +0300

    Добавил комментарии

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -48,7 +48,7 @@ class ArticlesController < ApplicationController
   private
 
   def article_params
-    params.require(:article).permit(:title, :text)
+    params.require(:article).permit(:title, :text, :name)
   end
 
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,5 @@
 class Article < ApplicationRecord
+    has_many :comments, dependent: :destroy
+    validates :title, presence: true
+    validates :text, presence: true
 end
