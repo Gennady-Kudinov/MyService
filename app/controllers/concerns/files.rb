594e545b8f2c952a17852964f3cdf0a501156aa0
Gennady-Kudinov <gennady71@gmail.com>
2022-10-30 16:13:37 +0300

    перед обновлением

--- app/controllers/concerns/files.rb
+++ app/controllers/concerns/files.rb
@@ -1,21 +1,21 @@
 module Files 
   extend ActiveSupport::Concern 
-  attr_accessor :file_name  
+  attr_accessor :file_name
   
   # загрузка файла 
-  def download_files 
+  def download_files
     send_file(current_path, filename: @file_name)
   end
   
   # определяем путь 
-  def current_path 
-    path = "D://BAZA/Files/"
+  def current_path
+    path = "D:/BAZA/Files/"
     path + search_file_name
   end
   
   # определяем подходящий файл здесь будет поиск 
-  def search_file_name 
-    @file_name = "HW8200598393_8200661124_SW8201010375_10144315AA_X7LLSRABH8H197909_E2.bin.zip"
+  def search_file_name
+     @file_name = "xxx.bin"
   end
   
   # если файл найден true  
