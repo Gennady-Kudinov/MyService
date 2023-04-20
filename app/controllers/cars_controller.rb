23bedcbc7c62ac5d0fd1fff0ebc4f1d6143036e1
Gennady-Kudinov <gennady71@gmail.com>
2023-04-20 18:23:51 +0300

    коррекция дерево папок

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -122,13 +122,13 @@ class CarsController < ApplicationController
     @soft_ecu = SoftEcu.find_by_id(car_params[:soft_ecu_id]).name
     @sw_ident = SwIdent.find_by_id(car_params[:sw_ident_id]).name
   
-    FileUtils.mkdir_p "F://BAZA/#{@brand_ecu}/#{@model_ecu}/#{@model}/#{@soft_ecu}/#{@sw_ident}/#{@licence.upcase}"
+    FileUtils.mkdir_p "F://BAZA/#{@brand_ecu}/#{@model_ecu}/#{@soft_ecu}/#{@sw_ident}/#{@licence.upcase}"
 
     @database_file = File.new('F://BAZA/database.txt', 'a+')
-    @database_file.puts "#{@licence.upcase}  #{@make}  #{@model}  Тип ЭБУ #{@brand_ecu} Модель ЭБУ #{@model_ecu} #{car_params[:mileage]}км.  Сумма #{car_params[:sum]} Телефон #{car_params[:phone]} Дата #{@datetime}"
+    @database_file.puts "#{@licence.upcase}  #{@make}  #{@model}  Тип ЭБУ #{@brand_ecu} Модель ЭБУ #{@model_ecu}  #{@soft_ecu}  #{@sw_ident} #{car_params[:mileage]}км.  Сумма #{car_params[:sum]} Телефон #{car_params[:phone]} Дата #{@datetime}"
     @database_file.close
 
-    @id_client = File.new("F://BAZA/#{@brand_ecu}/#{@model_ecu}/#{@model}/#{@soft_ecu}/#{@sw_ident}/#{@licence.upcase}/#{@licence.upcase}.html", 'a+')
+    @id_client = File.new("F://BAZA/#{@brand_ecu}/#{@model_ecu}/#{@soft_ecu}/#{@sw_ident}/#{@licence.upcase}/#{@licence.upcase}.html", 'a+')
     @id_client.puts "<body>#{@licence} #{@make} #{@model} #{@mileage}км. Тип ЭБУ #{@brand_ecu}: Модель ЭБУ #{@model_ecu}: Сумма #{car_params[:sum]} Телефон #{@phone} Дата #{@datetime}<br />#{@description}<br />#{@recommendation}<br /><body>"
     @id_client.close
   end
