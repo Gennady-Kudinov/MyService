ad6f004308a577280508281b788c8dac9986de54
Gennady-Kudinov <gennady71@gmail.com>
2023-07-05 07:33:05 +0300

    прогресс бар при отправке файла

---
+++ app/assets/javascripts/articles.js
@@ -0,0 +1,20 @@
+// app/assets/javascripts/articles.js
+
+$(document).on('ajax:send', '.new_article', function (event) {
+    var progressBar = $('.progress-bar');
+    progressBar.css('width', '0%');
+    progressBar.removeClass('bg-success');
+});
+
+$(document).on('ajax:progress', '.new_article', function (event) {
+    var progressBar = $('.progress-bar');
+    var progress = event.originalEvent.detail[0];
+    var percent = Math.round((progress.loaded * 100) / progress.total);
+    progressBar.css('width', percent + '%');
+});
+
+$(document).on('ajax:complete', '.new_article', function (event) {
+    var progressBar = $('.progress-bar');
+    progressBar.css('width', '100%');
+    progressBar.addClass('bg-success');
+});
